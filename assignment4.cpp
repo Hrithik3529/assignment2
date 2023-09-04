3a82f8344017230b71c857593221466d4675f043
Hrithik3529 <127133415+Hrithik3529@users.noreply.github.com>
2023-09-04 17:30:52 +0530

    Add files via upload

---
+++ assignment4.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int radius ;
+    cout<<"enter radius: ";
+    cin>>radius;
+    cout<<"enter height: ";
+    int height;
+    cin>>height;
+    int pi = 3.141592;
+    float volume;
+    volume=pi*radius*radius*height;
+    cout<<"volume of the cylinder is :"<<volume;
+
+
+}
---
+++ assignment5.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char ch1;
+    cout<<"enter the first character : ";
+    cin>>ch1;
+    char ch2;
+    cout<<"enter the second character: ";
+    cin>>ch2;
+    cout<<(ch1-ch2);
+    
+   
+}
